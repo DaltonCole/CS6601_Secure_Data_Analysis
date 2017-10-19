ebceede0f3bf6bf6905dd8da08f2a456183ca609
Wiz8910 <adamconaldbowers@gmail.com>
2017-10-19 17:00:44 -0500

    added multiplication

--- Project2/test.py
+++ Project2/test.py
@@ -9,31 +9,34 @@ def generateMultiplicationforNBitNumbers(term1,term2,numBits,maxNeededBits,run):
     #first have to extend to proper number of bits
     #text+= "sextend "+term1+" "+str(maxNeededBits)+"\n"
     #text+= "sextend "+term2+" "+str(maxNeededBits)+"\n"
-    text+= run+"formatted"+term2+" sextend "+term2+" "+str(maxNeededBits)+"\n"
     #now go bit by bit to get total
     for i in range(0,numBits):
         curLoop =str(i)
         text = text + run+ "curBit"+curLoop+" select "+ term1+" "+ str(i)+" "+str(i+1)+"\n"
         #now extend for 8 additions
-        text+= run+"curBitExt"+curLoop+" sextend "+run+"curBit"+curLoop+" "+str(numBits)+"\n"
+        #text+= run+"curBitExt"+curLoop+" concat " 0:"+str(numBits-1)+" "+run+"curBit"+curLoop+"\n"
+        text+= run+"curBitExt"+curLoop+" concat "
+        for k in range(0,numBits):
+            text+= run+ "curBit"+curLoop+ " "
+        text+="\n"
         #now have to add curBit to itself numbits time
+        '''
         for k in range(0,numBits):
             if (k == 0):
                 text += run+"additionResult"+curLoop+chr(ord('a') +k)+" select "+run+"curBitExt"+curLoop+" 0" + " " + str(numBits) + "\n"
             else:
                 text += run+"additionResult"+curLoop+chr(ord('a') +k)+" add " +run+"additionResult"+curLoop+chr(ord('a') +(k-1))\
                         + " "+run+"curBitExt"+curLoop+"\n"
-        text+=  run+"additionResult"+curLoop+chr(ord('a') +numBits)+" and " +run+"additionResult"+curLoop+chr(ord('a') +(numBits-1)) \
+        '''
+        text+=  run+"additionResult"+curLoop+chr(ord('a') +numBits)+" and " +run+"curBitExt"+curLoop \
                 + " " +term2 + "\n"
         if(i==0):
-            text+= run+"multiplicationResult"+curLoop+" sextend "+run+"additionResult"+curLoop+chr(ord('a') +numBits)\
-                   +" "+str(maxNeededBits)+"\n"
+            text+= run+"multiplicationResult"+curLoop+" concat 0:"+str(maxNeededBits-numBits)+" "+run+"additionResult"+curLoop+chr(ord('a') +numBits)\
+                   +"\n"
         else:
             #shift left by i and
-            text+= run + "additionResultShifted" + curLoop+chr(ord('a')+numBits) +" zextend "+" "+run + "additionResult"+ \
-                   curLoop + chr(ord('a') + numBits)+ " "+str(numBits+i) +"\n"
-            text+= run + "additionResultFinal" + curLoop+chr(ord('a')+numBits) + " sextend "+ \
-                   run + "additionResultShifted" + curLoop + chr(ord('a') + numBits) + " "+str(maxNeededBits) + "\n"
+            text+= run + "additionResultFinal" + curLoop+chr(ord('a')+numBits) + " concat 0:"+str(maxNeededBits-numBits-i)+" "+ \
+                   run + "additionResult" + curLoop + chr(ord('a') + numBits) + " 0:"+str(i) + "\n"
             #we add i zeroes then make sure same length for addition
             text+= run+"multiplicationResult"+curLoop+" add "+run+"multiplicationResult"+str(i-1)+\
                    " "+run + "additionResultFinal" + curLoop+ chr(ord('a') + numBits)+"\n"
@@ -42,8 +45,8 @@ def generateMultiplicationforNBitNumbers(term1,term2,numBits,maxNeededBits,run):
 
 
 def main():
-    numBitsForInput = 2
-    numTerms = 2
+    numBitsForInput = 5
+    numTerms = 3
     outText = ""
     maxNeededMultiplicationBits = 2*numBitsForInput
     maxNeededSumBits = maxNeededMultiplicationBits+int(math.ceil(math.log(maxNeededMultiplicationBits,2)))
@@ -63,11 +66,11 @@ def main():
         if(i==0):
             outText +="finalResultInter0 select "+chr(ord('a') +i)+"multiplicationResult"+str(numBitsForInput-1)+" 0 "+\
                        str(maxNeededMultiplicationBits) +"\n"
-            outText += "finalResult0 sextend "+"finalResultInter0 "+str(maxNeededSumBits)+"\n"
+            outText += "finalResult0 concat 0:"+str(maxNeededSumBits-maxNeededMultiplicationBits)+ " "+\
+                        chr(ord('a') +i)+"multiplicationResult"+str(numBitsForInput-1)+"\n"
         else:
-            outText += "finalResultInter"+str(i)+" select "+chr(ord('a') +i)+"multiplicationResult"+str(numBitsForInput-1)+" 0 "+\
-                       str(maxNeededMultiplicationBits) +"\n"
-            outText += "finalResultInterExt"+str(i)+" sextend finalResultInter"+str(i)+ " "+str(maxNeededSumBits)+"\n"
+            outText += "finalResultInterExt"+str(i)+" concat  0:"+str(maxNeededSumBits-maxNeededMultiplicationBits)+ " "+ \
+                       chr(ord('a') + i) + "multiplicationResult" + str(numBitsForInput - 1) + "\n"
             outText += "finalResult"+str(i)+" add finalResult"+str(i-1)+" finalResultInterExt"+str(i)+"\n"
 
     with open("test.cir", 'w') as f:
